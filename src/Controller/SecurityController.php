f727935a0591ddd322afe9b5a1380a3ac6007fb6
maxime <maximethi@hotmail.fr>
2019-07-30 16:39:40 +0200

    add config for connexion and registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -23,11 +28,40 @@ class SecurityController extends AbstractController
     }
 
     /**
+     * @Route("/inscription", name="registration")
+     */
+    public function registration(ObjectManager $manager, Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$user->getId()) {
+                $user->setDatesub(new \DateTime());
+            }
+            $user->setGrade(1);
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_login');
+
+        }
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
      * @Route("/logout", name="app_logout", methods={"GET"})
      */
     public function logout()
     {
         // controller can be blank: it will never be executed!
+        return $this->redirectToRoute('home');
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
+
 }
