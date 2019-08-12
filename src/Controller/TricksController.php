0a887b6026c24b66605aa8f2545ef9c92090c510
maxime <maximethi@hotmail.fr>
2019-08-12 12:01:00 +0200

    controller modifications to avoid extending abstractcontroller

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -7,51 +7,109 @@ use App\Entity\Figure;
 use App\Entity\Pictureslink;
 use App\Entity\User;
 use App\Form\FigureType;
+use App\Repository\FigureRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
+use Twig\Environment;
 
-class TricksController extends AbstractController
+class TricksController
 {
+    /** @var TricksController * */
+    private $trick;
+
+    /** @var Environment * */
+    private $templating;
+
+
+    public function __construct(FigureRepository $trick, Environment $templating)
+    {
+        $this->trick = $trick;
+        $this->templating = $templating;
+    }
+
     /**
      * @Route("/tricks", name="tricks")
      */
     public function index()
     {
-        return $this->render('tricks/index.html.twig', [
-            'controller_name' => 'TricksController',
-        ]);
+        $tricks = $this->trick->findAll();
+        return new Response($this->templating->render('tricks/index.html.twig', [
+            'tricks' => $tricks
+        ]));
     }
 
+
     /**
-     * @Route("/savetricks", name="savetricks2")
+     * @Route("/addtrick", name="addtrick")
      */
-    public function saveTricks(ObjectManager $manager , Request $request)
+    public function addTrick(ObjectManager $manager, Request $request)
     {
+        $user = $this->getUser();
         $figure = new Figure();
         $form = $this->createForm(FigureType::class, $figure);
         $form->handleRequest($request);
+        dump($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $manager->persist($figure);
             $manager->flush();
+            $this->addFlash('success', 'Votre figure a été ajouté');
 
-            return $this->redirectToRoute('savetricks');
+            return $this->redirectToRoute('tricks');
         }
-        return $this->render('tricks/index.html.twig', [
+        return $this->render('tricks/edittrick.html.twig', [
+            'figure' => $figure,
             'form' => $form->createView()
         ]);
     }
 
     /**
+     * @Route("/edit/{id}", name="edit.trick")
+     */
+    public function editTrick(Figure $figure, ObjectManager $manager, Request $request)
+    {
+        $form = $this->createForm(FigureType::class, $figure);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($figure);
+            $manager->flush();
+            $this->addFlash('success', 'Votre figure a été mise a jour');
+
+            return $this->redirectToRoute('tricks');
+        }
+        return $this->render('tricks/edittrick.html.twig', [
+            'figure' => $figure,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete.trick")
+     */
+    public function deleteTrick(Figure $figure, ObjectManager $manager)
+    {
+        //todo : boucle pour supp video et image associer
+        $manager->remove($figure);
+        $manager->flush();
+        $this->addFlash('success', 'Votre figure a été supprimé');
+
+        return $this->redirectToRoute('tricks');
+
+    }
+
+    /**
      * @Route("/trick/{id}", name="trick")
      */
-    public function getTrick($id)
+    public function getTrick($id, Figure $figure)
     {
-        $image = $this->getDoctrine()->getRepository(Pictureslink::class)->find($id);
+        //todo : verrifier si l'id existe pas (demander au prof)
+        $image = $this->getDoctrine()->getRepository(Pictureslink::class)->findBy(['figure' => $id]);
         $datatricks = $this->getDoctrine()->getRepository(Figure::class)->find($id);
-        $comments = $this->getDoctrine()->getRepository(Comments::class)->findBy(['idfigure' => $id ]);
-        dump($comments);
+        $comments = $this->getDoctrine()->getRepository(Comments::class)->findBy(['idfigure' => $id]);
+        dump($image);
         return $this->render('tricks/trick.html.twig', [
             'image' => $image,
             'data' => $datatricks,
