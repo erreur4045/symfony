04234ee86748f4b0fe21ca391753ce14662d601f
maxime <maximethi@hotmail.fr>
2020-01-24 18:47:44 -0500

    integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,59 +29,6 @@ $('.delete_modal').on('click', function () {
     openModal(slug,message);
 });
 
-$('.btn_load_tricks').on('click', function () {
-    $('#loader').css('background', 'transparent');
-    $('#loader').css('visibility', 'visible');
-    $('#tricks_list').css('pointer-events', 'none');
-    $('#btn_load_tricks').css('pointer-events', 'none');
-   let page = $(this).data('page');
-   let pagemax = $(this).data('pagemax');
-   let loadMore = $('#btn_load_tricks');
-   let url = loadMore.data('url')+'?page='+ page;
-   let tricks = $('#tricks_list');
-   $.ajax({
-       method: 'GET',
-       url: url,
-       success: function (response) {
-           tricks.append(response);
-           loadMore.data('page', page + 1 );
-           document.getElementById("loader").style.visibility = "hidden";
-           $('#tricks_list').css('pointer-events', 'all');
-           $('#btn_load_tricks').css('pointer-events', 'all');
-           if (page >= pagemax){
-               $('.btn_load_tricks').remove();
-           }
-       }
-   });
-});
-
-$('.btn_load_coms').on('click', function () {
-    $('#loader').css('background', 'transparent');
-    $('#loader').css('visibility', 'visible');
-    $('#coms_list').css('pointer-events', 'none');
-    $('#btn_load_coms').css('pointer-events', 'none');
-    let page = $(this).data('page');
-    let pagemax = $(this).data('pagemax');
-    let figureId = $(this).data('figureid');
-    let loadMore = $('#btn_load_coms');
-    let url = loadMore.data('url')+'?page='+ page+'&figureid='+figureId;
-    let coms = $('#coms_list');
-    $.ajax({
-        method: 'GET',
-        url: url,
-        success: function (response) {
-            coms.append(response);
-            loadMore.data('page', page + 1 );
-            document.getElementById("loader").style.visibility = "hidden";
-            $('#coms_list').css('pointer-events', 'all');
-            $('#btn_load_coms').css('pointer-events', 'all');
-            if (page >= pagemax){
-                $('.btn_load_coms').remove();
-            }
-        }
-    });
-});
-
 $('#btn_see_media').on('click',function () {
     document.getElementById("carousel_mobile").style.display = "block";
     document.getElementById("btn_see_media").style.display = "none";
