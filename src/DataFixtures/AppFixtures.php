61aa5500ed2407c7e27ce7c532c599f605087c99
maxime <maximethi@hotmail.fr>
2020-01-29 17:10:26 -0500

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -60,80 +60,167 @@ class AppFixtures extends Fixture
             4 => 'https://www.youtube.com/embed/SQyTWk7OxSI'
         ];
 
+        $figureDatas = [
+          0 => [
+              'titre' => 'Mute',
+              'desciption' => 'saisie de la carre frontside de la planche entre les deux pieds avec la main avant.',
+              'categorie' => 'Les grabs'
+                ],
+            1 => [
+                'titre' => '360',
+                'desciption' => 'trois six pour un tour complet.',
+                'categorie' => 'Les rotations'
+            ],
+            2 => [
+                'titre' => 'Japan air',
+                'desciption' => 'Saisie de l\'avant de la planche, avec la main avant, du côté de la carre frontside.',
+                'categorie' => 'Les grabs'
+            ],
+            3 => [
+                'titre' => '1080',
+                'desciption' => 'trois tours complets',
+                'categorie' => 'Les rotations'
+            ],
+            4 => [
+                'titre' => 'Back flips',
+                'desciption' => 'Rotations en arrière',
+                'categorie' => 'Les rotations'
+            ],
+            5 => [
+                'titre' => 'Rodeo',
+                'desciption' => 'Figure tête en bas où l’athlète pivote en diagonale au-dessus de son épaule pendant qu’il fait un salto',
+                'categorie' => 'Les rotations désaxées'
+            ],
+            6 => [
+                'titre' => 'Rocket air',
+                'desciption' => 'Figure aérienne où le surfeur saisit la carre pointe du pied à l’avant du pied avant avec la main avant, la jambe est redressée et la planche pointe perpendiculairement au sol',
+                'categorie' => 'Old school'
+            ],
+            7 => [
+                'titre' => 'Seat belt',
+                'desciption' => 'Figure aérienne où le surfeur saisit le talon de la planche de surf avec sa main avant pendant que la jambe avant est tendue.',
+                'categorie' => 'Les grabs'
+            ],
+            8 => [
+                'titre' => 'Truck driver',
+                'desciption' => 'saisie du carre avant et carre arrière avec chaque main (comme tenir un volant de voiture)',
+                'categorie' => 'Les grabs'
+            ],
+            9 => [
+                'titre' => 'Stalefish',
+                'desciption' => ' Figure aérienne où l’athlète saisit la carre côté talons derrière la jambe arrière avec la main arrière pendant que la jambe arrière est redressée.',
+                'categorie' => 'Les grabs'
+            ]
+        ];
+
+        $RandComments = [
+            'Trop bien cette figure',
+            'C\'est magnifique',
+            'C\'est ma figure !',
+            'Top',
+            'Comment on peut faire ça !? ^^',
+            'Polalalalal !',
+            'Impressionant !',
+            'WTF!',
+            'Completement fou :) ',
+            'waw',
+            'WAOUWWWWW'
+        ];
+
         foreach ($figureGroupeNames as $name) {
             $figuregroupe = new Figuregroup();
             $figuregroupe->setName($name);
             $manager->persist($figuregroupe);
         }
 
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < 4; $i++) {
             $user = new User();
             $user->setProfilePicture(Pictureslink::PICTURELINKUSERRAND)
                 ->setDatesub($faker->dateTimeInInterval('-30 days', '+5 dayes'))
-                ->setName($faker->lastName())
+                ->setName($faker->firstName())
                 ->setMail($faker->safeEmail)
                 ->setPassword($this->encoder->encodePassword($user, "testpass"));
-                    $manager->persist($user);
-            for ($k = 0; $k <= 2; $k++) {
-                $figure = new  Figure();
-                $figure->setName($faker->sentence(3))
-                    ->setUser($user)
-                    ->setDatecreate(
-                        $faker->dateTimeInInterval('-30 days', '+5 dayes')
-                    )
-                    ->setDescription(implode(" ", $faker->sentences(4)))
-                    ->setIdfiguregroup($figuregroupe);
-                $manager->persist($figure);
-                for ($l = 0; $l == 0; $l++) {
-                        $filesystem = new Filesystem();
-                        $pictureDefault = new Pictureslink();
-                        $randId = rand(0, 2);
-                        $randPicture = Pictureslink::PICTURELINKTRICKRAND[$randId];
-                        $newPicture = $randPicture . '-' . uniqid() . '.jpg';
-                        $filesystem->copy(
-                            $this->tricksPicturesDirectory . $randPicture,
-                            $this->tricksPicturesDirectory . $newPicture
-                        );
-                        $pictureDefault->setAlt($faker->sentence)
-                            ->setFirstImage(1)
-                            ->setFigure($figure)
-                            ->setLinkpictures($newPicture);
-                        $manager->persist($pictureDefault);
-                }
-                for ($l = 0; $l <= 2; $l++) {
-                    $filesystem = new Filesystem();
-                    $picture = new Pictureslink();
-                    $randId = rand(0, 2);
-                    $randPicture = Pictureslink::PICTURELINKTRICKRAND[$randId];
-                    $newPicture = $randPicture . '-' . uniqid() . '.jpg';
-                    $filesystem->copy(
-                        $this->tricksPicturesDirectory . $randPicture,
-                        $this->tricksPicturesDirectory . $newPicture
+                $manager->persist($user);
+
+        }
+        /** @var EntityManagerInterface  $manager */
+        $manager->flush();
+
+        /** @var User $allUser */
+        $allUser = $manager->getRepository(User::class)->findAll();
+
+        /**
+         * table containing all user ids to randomize comment creation on line 169
+         * @var array $userIds
+         */
+        $userIds = [];
+        foreach ($allUser as $userId){
+            array_push($userIds, $userId->getId());
+        }
+        foreach ($figureDatas as $figureData)
+        {
+            $figure = new  Figure();
+            $figure->setUser($user)
+                ->setName($figureData['titre'])
+                ->setDatecreate($faker->dateTimeInInterval('-30 days', '+5 dayes'))
+                ->setIdfiguregroup(
+                    $manager->getRepository(Figuregroup::class)
+                        ->findOneBy(['name' => $figureData['categorie']])
+                )
+                ->setDescription($figureData['desciption']);
+            $manager->persist($figure);
+            for ($l = 0; $l == 0; $l++) {
+                $filesystem = new Filesystem();
+                $pictureDefault = new Pictureslink();
+                $randId = rand(0, 2);
+                $randPicture = Pictureslink::PICTURELINKTRICKRAND[$randId];
+                $newPicture = $randPicture . '-' . uniqid() . '.jpg';
+                $filesystem->copy(
+                    $this->tricksPicturesDirectory . $randPicture,
+                    $this->tricksPicturesDirectory . $newPicture
+                );
+                $pictureDefault->setAlt($faker->sentence)
+                    ->setFirstImage(1)
+                    ->setFigure($figure)
+                    ->setLinkpictures($newPicture);
+                $manager->persist($pictureDefault);
+            }
+            for ($l = 0; $l <= 2; $l++) {
+                $filesystem = new Filesystem();
+                $picture = new Pictureslink();
+                $randId = rand(0, 2);
+                $randPicture = Pictureslink::PICTURELINKTRICKRAND[$randId];
+                $newPicture = $randPicture . '-' . uniqid() . '.jpg';
+                $filesystem->copy(
+                    $this->tricksPicturesDirectory . $randPicture,
+                    $this->tricksPicturesDirectory . $newPicture
+                );
+                $picture->setAlt($faker->sentence)
+                    ->setFirstImage(0)
+                    ->setFigure($figure)
+                    ->setLinkpictures($newPicture);
+                $manager->persist($picture);
+            }
+            for ($m = 0; $m <= 2; $m++) {
+                $video = new Videolink();
+                $video->setFigure($figure)
+                    ->setLinkvideo(
+                        $videosYoutube[array_rand($videosYoutube)]
                     );
-                    $picture->setAlt($faker->sentence)
-                        ->setFirstImage(0)
-                        ->setFigure($figure)
-                        ->setLinkpictures($newPicture);
-                    $manager->persist($picture);
-                }
-                for ($m = 0; $m <= 2; $m++) {
-                            $video = new Videolink();
-                            $video->setFigure($figure)
-                                ->setLinkvideo(
-                                    $videosYoutube[array_rand($videosYoutube)]
-                                );
-                            $manager->persist($video);
-                }
-                for ($n = 0; $n <= 5; $n++) {
-                    $comment = new Comments();
-                    $comment->setDatecreate(
-                        $faker->dateTimeInInterval('-30 days', '+5 dayes')
+                $manager->persist($video);
+            }
+            for ($n = 0; $n <= 5; $n++) {
+                $comment = new Comments();
+                $comment->setDatecreate(
+                    $faker->dateTimeInInterval('-30 days', '+5 dayes')
+                )
+                    ->setUser(
+                        $manager->getRepository(User::class)
+                            ->findOneBy(['id' => $userIds[array_rand($userIds)]])
                     )
-                        ->setUser($user)
-                        ->setIdfigure($figure)
-                        ->setText(implode(" ", $faker->sentences(2)));
-                    $manager->persist($comment);
-                }
+                    ->setIdfigure($figure)
+                    ->setText($RandComments[array_rand($RandComments)]);
+                $manager->persist($comment);
             }
         }
         $manager->flush();
