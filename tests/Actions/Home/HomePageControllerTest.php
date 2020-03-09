02d97f5ba3bb0111debc22a2abc61bf68547bcb5
maxime <maximethi@hotmail.fr>
2020-03-09 12:35:05 -0500

    last fixs

--- tests/Actions/Home/HomePageControllerTest.php
+++ tests/Actions/Home/HomePageControllerTest.php
@@ -15,28 +15,40 @@ namespace App\Tests\Actions\Home;
 use App\DataFixtures\AppFixtures;
 use App\Tests\AbstractTestCase;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class HomePageControllerTest extends AbstractTestCase
+class HomePageControllerTest extends WebTestCase
 {
-    /** @var AppFixtures */
-    protected $fixture;
-    /** @var ObjectManager */
-    protected $manager;
-
-    protected function setUp()
-    {
-        parent::setUp();
-        $container = self::$container;
-        $manager = $container->get('doctrine.orm.entity_manager');
-        $this->fixture->load($manager);
-    }
-
     public function testIndex()
     {
         $this->ensureKernelShutdown();
         $client = static::createClient();
         $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexTrick()
+    {
+        $this->ensureKernelShutdown();
+        $client = static::createClient();
+        $client->request('GET', '/#tricks');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexBadAncre()
+    {
+        $this->ensureKernelShutdown();
+        $client = static::createClient();
+        $client->request('GET', '/#sdfs');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexBadRoute()
+    {
+        $this->ensureKernelShutdown();
+        $client = static::createClient();
+        $client->request('GET', '/sdf');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 }
